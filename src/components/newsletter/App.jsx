5a1af30b439e95ff0c1a1ffde2da90dc6816a170
kellycesario <kellycesario@gmail.com>
2022-05-12 11:54:12 -0300

    Criação do componente newsletter

---
+++ src/components/newsletter/App.jsx
@@ -0,0 +1,14 @@
+import './styles/Global.css'
+import Index from './components/newsletter/Index.jsx'
+
+function App() {
+
+
+  return (
+    <> 
+      <Index />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/newsletter/Index.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './NewsletterSyle.css';
+
+const SignNewsletter = () => {
+
+    return (
+
+            <div className="formStyleButton">
+                    <button type="submit" className="btn"> Submit </button>
+                </div>
+
+};
+
+export default Index;
