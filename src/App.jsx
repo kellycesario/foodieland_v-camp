fcc82fdd104e01d2e0c5c1dfcb3f48678021db3d
andradeviniicius <viniciusdandrade01@gmail.com>
2022-05-12 18:19:59 -0300

    fixing file name to avoid confusing over html elem <footer>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "../public/styles/Global.css";
-import MainFooter from './components/footer'
+import MainFooter from './components/MainFooter'
 import MainHeader from "./components/header";
 import ContactForm from "./components/ContactForm";
 import SignNewsletter from "./components/SignNewsletter";
