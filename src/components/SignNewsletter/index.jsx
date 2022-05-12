94b5e30ac554b40c4424b50bc70efea67af47a9b
kellycesario <kellycesario@gmail.com>
2022-05-12 15:29:40 -0300

    Sign Newsletter finalizado

--- src/components/SignNewsletter/index.jsx
+++ src/components/SignNewsletter/index.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewsletterStyle.css';
 import emailBackgroundMobile from './../../assets/img/emailBackgroundMobile.png';
 
 const SignNewsletter = () => {
+    
+    const submitNewsHandler = (event) => {
+        event.preventDefault();
+        console.log(event);
+    }
+    
     return (
         <section>
             <div>
-                <img src={emailBackgroundMobile} alt="" />
-            </div>
-
-            <div>
                 <h1>Deliciousness to your inbox</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, 
+                <p className="inviteText">Lorem ipsum dolor sit amet, consectetuipisicing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore 
                     magna aliqut enim ad minim </p>
             </div>
+            <form onSubmit={submitNewsHandler}>
+                <div className="insertEmail">
+                    <input type="email" className="input" placeholder="Your email address..." required/>
+                </div>
 
-            <div>
-                <input type="email" placeholder="Your email address..."/>
-            </div>
-
-            <div className="formStyleButton">
+                <div className="styleButton">
                     <button type="submit" className="btn"> Submit </button>
-            </div>
+                </div>
+        </form>
         </section>
     )
 
