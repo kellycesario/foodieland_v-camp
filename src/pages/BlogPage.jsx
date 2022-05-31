32a4679bb76789a4d47fe34ed73adc1e629a4b9b
NatanaelSilva <natanael.silva@valtech.com>
2022-05-31 11:55:38 -0300

    pagiant

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,11 @@
+import Pagination from "../components/Pagination";
+
+export default function BlogPage() {
+  return (
+    <>
+      <Pagination />
+      <Component2 />
+      <Component3 />
+    </>
+  );
+}
