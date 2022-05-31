e8b89cc7a6f0c94067535a59e59cf5670c413592
andradeviniicius <viniciusdandrade01@gmail.com>
2022-05-31 11:05:30 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import MainFooter from './components/MainFooter'
 import MainHeader from "./components/header";
 import ContactForm from "./components/ContactForm";
 import SignNewsletter from "./components/SignNewsletter";
+import SearchBlogAndArticle from "./components/SearchBlogAndArticle";
 
 function App() {
   return (
     <>
-      <MainHeader />
-      <ContactForm />
-      <SignNewsletter />
-      <CarrouselFood/>
-      <MainFooter/>
+      {/* <MainHeader /> */}
+      {/* <ContactForm /> */}
+      {/* <SignNewsletter /> */}
+      {/* <CarrouselFood/> */}
+      {/* <MainFooter/> */}
+      <SearchBlogAndArticle/>
     </>
   );
 }
