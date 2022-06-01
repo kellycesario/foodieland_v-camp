87ae33d4dd74db0bd42437bd44929d62383cb335
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-01 13:40:29 -0300

    btn minimal complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,16 @@ import BlogAndArticle from "./components/BlogAndArticle";
 import Contact from "./pages/Contact";
 import BlogPage from "./pages/BlogPage";
 
+// Temp
+import MainButton from "./components/MainButton";
+// Temp
+
 import "../public/styles/Global.css";
 
 function App() {
   return (
     <>
-      <MainHeader />
+      {/* <MainHeader />
 
       <Route path="/contact">
         <Contact />
@@ -28,7 +32,28 @@ function App() {
         <SignNewsletter/>
       </Route>
 
-      <MainFooter />
+      <MainFooter /> */}
+      <MainButton Color="dark" Content="Submit" />
+      <MainButton Color="dark" Content="Search" />
+      <MainButton Color="dark" Content="Newsletter" />
+
+      <MainButton Color="light" Content="Large Test" Size={"large tiny"} />
+
+      <div style={{ display: "flex", gap:"5px" }}>
+        <MainButton Color="dark" Size="tiny" Content="1" />
+        <MainButton Color="light" Size="tiny" Content="2" />
+        <MainButton Color="light" Size="tiny" Content="3" />
+        <MainButton Color="light" Size="tiny" Content="4" />
+        <MainButton Color="light" Size="tiny" Content="5" />
+      </div>
+
+      {/* 
+    btnSize         = default/large
+    btnColor        = default/light
+    btnInsideInput  = true/false
+    btnMinimal      = true/false
+    content         = string
+    */}
     </>
   );
 }
