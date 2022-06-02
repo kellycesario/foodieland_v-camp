d669008e04683558ca317b155fd6dad35e274187
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-02 18:36:30 -0300

    general fixes

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import MainButton from "../MainButton";
-import './style.css'
+import "./style.css";
 
 const Pagination = ({ sendPage }) => {
   const [currentPage, setCurrentPage] = useState(0);
@@ -14,13 +14,56 @@ const Pagination = ({ sendPage }) => {
     <>
       <div className="buttonArea">
         <div className="buttonList">
-          <MainButton Content={1} value={0} Size="tiny" Color="light" />
-          <MainButton Content={2} value={1} Size="tiny" Color="light" />
-          <MainButton Content={3} value={2} Size="tiny" Color="light" />
-          <MainButton Content={4} value={2} Size="tiny" Color="light" />
-          <MainButton Content={5} value={2} Size="tiny" Color="light" />
-          <MainButton Content={'...'} value={2} Size="tiny" Color="light" />
-          <MainButton Content={'>'} value={2} Size="tiny" Color="light" />
+          {/* Mapear MainButton pra renderizar o suficiente pra qtd de pagina */}
+          <MainButton
+            Content={1}
+            Value={0}
+            Size="tiny"
+            Color="light"
+            handleClick={handleClick}
+          />
+          <MainButton
+            Content={2}
+            Value={1}
+            Size="tiny"
+            Color="light"
+            handleClick={handleClick}
+          />
+          <MainButton
+            Content={3}
+            Value={2}
+            Size="tiny"
+            Color="light"
+            handleClick={handleClick}
+          />
+          <MainButton
+            Content={4}
+            Value={3}
+            Size="tiny"
+            Color="light"
+            handleClick={handleClick}
+          />
+          <MainButton
+            Content={5}
+            Value={4}
+            Size="tiny"
+            Color="light"
+            handleClick={handleClick}
+          />
+          <MainButton
+            Content={"..."}
+            Value={2}
+            Size="tiny"
+            Color="light"
+            handleClick={handleClick}
+          />
+          <MainButton
+            Content={">"}
+            value={2}
+            Size="large"
+            Color="light"
+            handleClick={handleClick}
+          />
         </div>
       </div>
     </>
