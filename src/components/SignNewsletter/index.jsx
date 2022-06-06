4d5d2ba188b761be2ceca73b55f602d9a5a287c9
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-06 13:39:27 -0300

    Call Review CSS SignNewsLetter Card

--- src/components/SignNewsletter/index.jsx
+++ src/components/SignNewsletter/index.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from "react";
-import "./NewsletterStyle.css";
+
 import SubmitButton from "./../ContactForm/SubmitButton";
 import InputField from "./../ContactForm/InputField";
-import emailBackgroundMobile from "../../../public/assets/img/emailBackgroundMobile.png";
+import MainButton from "../MainButton";
+
 import emailBackground from "../../../public/assets/img/emailBackground.png";
+import emailBackgroundMobile from "../../../public/assets/img/emailBackgroundMobile.png";
+
+import "./NewsletterStyle.css";
 
 const SignNewsletter = () => {
-    const submitNewsHandler = (event) => {
-      event.preventDefault();
-      alert("Now you are subscribed!")
-      event.target.reset();
-    };
+  const submitNewsHandler = (event) => {
+    event.preventDefault();
+    alert("Now you are subscribed!");
+    event.target.reset();
+  };
 
   return (
-    <section className="newsletter__container">
+    <section className="newsletter">
       <section className="newsletter__image">
-        <div>
-          <h1 className="newsletter__image--title">Deliciousness to your inbox</h1>
-          <p className="newsletter__invite-text">
-            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
-          </p>
-        </div>
-        <div>
-          <form className="newsletter__form" onSubmit={submitNewsHandler}>
-            <label for="input__email" />
-            <input name="input__newsletter" type="email" className="input__email--newsletter" placeholder="Your email address..." />
-            <button type="submit" className="button__submit newsBtn">Subscribe</button>
-          </form>
-        </div>
+        <h1 className="newsletter__imageTitle">Deliciousness to your inbox</h1>
+        <p className="newsletter__imageText">
+          Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+        </p>
+        <form className="newsletter__form" onSubmit={submitNewsHandler}>
+          <label for="input__email" />
+          <input
+            name="input__newsletter"
+            type="email"
+            className="newsletter__input"
+            placeholder="Your email address..."
+          />
+          {/* <MainInput InsideInput={true}></MainInput> */}
+          <MainButton InsideInput={true} Content={"Submit"} Size={"large"}></MainButton>
+        </form>
       </section>
     </section>
   );
