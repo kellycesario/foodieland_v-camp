70460450c24d72a61f5874483a895807e4bf882f
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-13 12:40:53 -0300

    initial commit

---
+++ src/components/Molecules/FeaturedRecipeCard/index.jsx
@@ -0,0 +1,10 @@
+export default function FeaturedRecipeCard(props) {
+  return (
+    <>
+      <div className="featuredRecipeCard">
+        <div className="featuredRecipeCard__leftContainer"></div>
+        <div className="featuredRecipeCard__rightContainer"></div>
+      </div>
+    </>
+  );
+}
