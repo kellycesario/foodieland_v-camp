8d33b86b691f2c6f07b098f18e2d7e1cccffc11d
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-14 18:49:48 -0300

    Carrousle Btn implemented

--- src/components/Organisms/FeaturedRecipe/index.jsx
+++ src/components/Organisms/FeaturedRecipe/index.jsx
@@ -1,8 +1,13 @@
 import FeaturedRecipeCard from "@molecules/FeaturedRecipeCard";
+import ArrowLeft from "@icons/arrowLeft.svg";
+import ArrowRight from "@icons/arrowRight.svg";
 
 export default function FeaturedRecipe() {
   return (
     <>
+      <button className="btn btn--light btn--rounded btn--tiny">
+        <img src={ArrowLeft} alt="" />
+      </button>
       <ul>
         <li>
           <FeaturedRecipeCard />
@@ -14,6 +19,9 @@ export default function FeaturedRecipe() {
           <FeaturedRecipeCard />
         </li>
       </ul>
+      <button>
+        <img src={ArrowRight} alt="" />
+      </button>
     </>
   );
 }
