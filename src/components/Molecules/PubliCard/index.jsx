a3c96d3612129077746630959724f32db631f510
kellycesario <kellycesario@gmail.com>
2022-06-14 18:33:36 -0300

    Creating the 'PubliCard' molecule

---
+++ src/components/Molecules/PubliCard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PubliCard = (props) => {
+
+    return (
+        <section>
+            <img src={props.img} />
+        </section>
+    );
+}
+
+export default PubliCard;
