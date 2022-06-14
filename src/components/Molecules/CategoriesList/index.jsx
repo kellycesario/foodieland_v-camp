d6623055b460e3242c2c789011e5955cc45485fa
JrFogassi <jrfogassi@gmail.com>
2022-06-14 16:11:29 -0300

    starting the CategoriesList component CSS, deleting the GlobalCarrousel component and improving the cards list and props

--- src/components/Molecules/CategoriesList/index.jsx
+++ src/components/Molecules/CategoriesList/index.jsx
@@ -1,35 +1,61 @@
 import React from "react";
-import CardsProps from "../../Atoms/CategoriesCards/cardsProps";
-import MainCarrousel from "../../Context/MainCarrousel";
+import CategoriesCards from "../../Atoms/CategoriesCards";
+
+
 
 // importando as imagens dos alimentos
-import sushiRice from "../../../../public/assets/img/sushiRice.png";
-import lettuce from "../../../../public/assets/img/lettuce.png";
-import steakRaw from "../../../../public/assets/img/steakRaw.png";
-import cakePiece from "../../../../public/assets/img/cakePiece.png";
-import sanduich from "../../../../public/assets/img/sanduich.png";
-import chocolateBar from "../../../../public/assets/img/chocolateBar.png";
+import sushiRice from "@img/sushiRice.png";
+import lettuce from "@img/lettuce.png";
+import steakRaw from "@img/steakRaw.png";
+import cakePiece from "@img/cakePiece.png";
+import sanduich from "@img/sanduich.png";
+import chocolateBar from "@img/chocolateBar.png";
 
-//importando o fundo das imagens
-import sushiRiceBackground from "../../../../public/assets/img/sushiRiceBackground.png";
-import lettuceBackground from "../../../../public/assets/img/lettuceBackground.png";
-import steakRawBackground from "../../../../public/assets/img/steakRawBackground.png";
-import cakePieceBackground from "../../../../public/assets/img/cakePieceBackground.png";
-import sanduichBackground from "../../../../public/assets/img/sanduichBackground.png";
-import chocolateBarBackground from "../../../../public/assets/img/chocolateBarBackground.png";
+// importando o fundo das imagens
+import sushiRiceBackground from "@img/sushiRiceBackground.png";
+import lettuceBackground from "@img/lettuceBackground.png";
+import steakRawBackground from "@img/steakRawBackground.png";
+import cakePieceBackground from "@img/cakePieceBackground.png";
+import sanduichBackground from "@img/sanduichBackground.png";
+import chocolateBarBackground from "@img/chocolateBarBackground.png";
 
 
 
-const CategoriesCards = () => {
+const CategoriesList = () => {
     return (
         <>
-            <MainCarrousel 
-                mainImage={sushiRice}
-                secondaryImage={sushiRiceBackground}
+            <CategoriesCards
+                foodSymbol = {sushiRice}
+                foodBackground = {sushiRiceBackground}
+                title= "Breakfest"
             />
-
+            <CategoriesCards
+                foodSymbol = {lettuce}
+                foodBackground = {lettuceBackground}
+                title= "Vegan"
+            />
+            <CategoriesCards
+                foodSymbol = {steakRaw}
+                foodBackground = {steakRawBackground}
+                title= "Meat"
+            />
+            <CategoriesCards
+                foodSymbol = {cakePiece}
+                foodBackground = {cakePieceBackground}
+                title= "Dessert"
+            />
+            <CategoriesCards
+                foodSymbol = {sanduich}
+                foodBackground = {sanduichBackground}
+                title= "Lunch"
+            />
+            <CategoriesCards
+                foodSymbol = {chocolateBar}
+                foodBackground = {chocolateBarBackground}
+                title= "Chocolate"
+            />
         </>
     )
 }
 
-export default CategoriesCards
+export default CategoriesList
