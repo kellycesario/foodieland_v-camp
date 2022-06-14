71daefd5fdaa557d4c955449ab2f22472dc8c39b
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-14 18:26:35 -0300

    Removing local dev things

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -2,18 +2,14 @@ import MoreRecipes from "@organisms/MoreRecipes";
 import CategoriesSection from "@organisms/CategoriesSection";
 import FeaturedRecipeCard from "../../Molecules/FeaturedRecipeCard";
 import LearnMore from "../../Organisms/LearnMore";
-import CarrouselBtn from "../../Atoms/CarrouselBtn";
 
 const Home = () => {
   return (
     <>
-      <CarrouselBtn />
-      <button className="btn btn--light btn--tiny">{'<'}</button>
-      <button className="btn btn--rounded btn--light btn--tiny">{'>'}</button>
-      {/* <FeaturedRecipeCard />
+      <FeaturedRecipeCard />
       <CategoriesSection />
       <LearnMore />
-      <MoreRecipes />; */}
+      <MoreRecipes />;
     </>
   );
 };
