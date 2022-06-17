7731205c6d97d70199419f0c1952bdffc257537f
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-17 12:24:40 -0300

    Recipetime and categor implemented on featuredCard

--- src/components/Molecules/FeaturedRecipeCard/index.jsx
+++ src/components/Molecules/FeaturedRecipeCard/index.jsx
@@ -1,6 +1,7 @@
 import MainButton from "@atoms/MainButton";
 
-import featuredCardRecipe from '@img/featuredCardRecipe.png';
+import RecipeTime from "../../Atoms/RecipeTime";
+import RecipeCategorie from "../../Atoms/RecipeCategorie";
 
 import "./style.css";
 
@@ -10,18 +11,25 @@ export default function FeaturedRecipeCard() {
       <div className="featuredRecipeCard">
         <div className="featuredRecipeCard__leftContainer">
           <div className="featuredRecipecard__categoriesStamp"></div>
-          <h1 className="featuredRecipeCard__title">Spicy Delicious Chicken Wings</h1>
+          <h1 className="featuredRecipeCard__title">
+            Spicy Delicious Chicken Wings
+          </h1>
           <p className="featuredRecipeCard__subTitle">
             Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
           </p>
-          <span className="recipeTime"></span>
-          <span className="recipeCategorie"></span>
+          <div className="featuredRecipeCard__recipeInfo">
+            <RecipeTime time={30} />
+            <RecipeCategorie />
+          </div>
           <span className="recipeAuthor"></span>
           <MainButton></MainButton>
         </div>
         <div className="featuredRecipeCard__badge">
-          <img src="../../../../public/assets/img/featuredRecipeBadge.png" alt="" />
+          <img
+            src="../../../../public/assets/img/featuredRecipeBadge.png"
+            alt=""
+          />
         </div>
         <div className="featuredRecipeCard__rightContainer">
           {/* <img src={featuredCardRecipe} alt="" /> */}
