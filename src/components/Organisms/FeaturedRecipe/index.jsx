852798b3ad4a9b3d56b73be6b197d8327b88cdff
andradeviniicius <viniciusdandrade01@gmail.com>
2022-06-17 10:25:48 -0300

    initial featuredRecipeCard Structure

--- src/components/Organisms/FeaturedRecipe/index.jsx
+++ src/components/Organisms/FeaturedRecipe/index.jsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 import FeaturedRecipeCard from "@molecules/FeaturedRecipeCard";
 import ArrowLeft from "@icons/arrowLeft.svg";
 import ArrowRight from "@icons/arrowRight.svg";
@@ -6,18 +8,37 @@ import MainButton from "@atoms/MainButton";
 import styles from "./style.module.css";
 
 export default function FeaturedRecipe() {
+  const carrouselRef = useRef(null);
+
+  function scrollLeft() {
+    carrouselRef.current.scrollBy(-350, 0);
+  }
+
+  function scrollRight() {
+    carrouselRef.current.scrollBy(350, 0);
+  }
+
+  /*
+  el.scrollBy
+  Para descobrir coordenadas utilizar
+  
+  evento de rolagem
+  
+  Element.clientWidth
+  Element.scrollLeft
+  Element.scrollWidth
+  */
   return (
     <>
       <div className={styles.mainContainer}>
-        <div className={styles.mainContainer__btnContainer}>
-          <MainButton
-            Size={"tiny"}
-            Color={"light"}
-            Type={"rounded"}
-            Content={"<"}
-          />
-        </div>
-        <ul className={styles.mainContainer__list}>
+        <MainButton
+          handleClick={scrollLeft}
+          Size={"tiny"}
+          Color={"light"}
+          Type={"rounded"}
+          Content={"<"}
+        />
+        <ul ref={carrouselRef} className={styles.mainContainer__list}>
           <li className={styles.mainContainer__item}>
             <FeaturedRecipeCard />
           </li>
@@ -29,6 +50,7 @@ export default function FeaturedRecipe() {
           </li>
         </ul>
         <MainButton
+          handleClick={scrollRight}
           className={styles.mainContainer__btnLeft}
           Size={"tiny"}
           Color={"light"}
