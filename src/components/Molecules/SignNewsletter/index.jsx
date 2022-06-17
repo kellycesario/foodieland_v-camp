32ddeb19b0500d3e6defb0bb6534bef55e0acb8f
kellycesario <kellycesario@gmail.com>
2022-06-16 21:10:42 -0300

    Importing the signnewsletter molecule and creating the newsletter organism

---
+++ src/components/Molecules/SignNewsletter/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import "./style.css";
+
+const SignNewsletter = () => {
+  const submitNewsHandler = (event) => {
+    event.preventDefault();
+    alert("Now you are subscribed!");
+    event.target.reset();
+  };
+
+  return (
+    <section className="newsletter">
+      <section className="newsletter__image">
+        <h1 className="newsletter__imageTitle">Deliciousness to your inbox</h1>
+        <p className="newsletter__imageText">
+          Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+        </p>
+        <form className="newsletter__form" onSubmit={submitNewsHandler}>
+          <label for="input__email" />
+          <input
+            name="input__newsletter"
+            type="email"
+            className="newsletter__input"
+            placeholder="Your email address..."
+          />
+          <button className="newsletter__button btn btn--large btn--insideInput">Subscribe</button>
+        </form>
+      </section>
+    </section>
+  );
+};
+
+export default SignNewsletter;
