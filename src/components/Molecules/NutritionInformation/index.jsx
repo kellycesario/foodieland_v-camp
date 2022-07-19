97c87f080b37fb5fb0a18164fb118e697066d214
kellycesario <kellycesario@gmail.com>
2022-07-19 18:34:26 -0300

    Making the 'Recipes' component responsive (static version)

--- src/components/Molecules/NutritionInformation/index.jsx
+++ src/components/Molecules/NutritionInformation/index.jsx
@@ -9,23 +9,23 @@ const NutriInformation = ()  => {
         <ul className="nutriInformation__contents">
             <li className="nutriInformation__item">Calories</li>
             <li className="nutriInformation__info">219.9 kcal</li>
-            <img src={line} />
+            <img className="nutriInformation__breakLine" src={line} />
           
             <li className="nutriInformation__item">Total Fat</li>
             <li className="nutriInformation__info">10.7 g</li>
-            <img src={line} />
+            <img className="nutriInformation__breakLine" src={line} />
             
             <li className="nutriInformation__item">Protein</li>
             <li className="nutriInformation__info">7.9 g</li>
-            <img src={line} />
+            <img className="nutriInformation__breakLine" src={line} />
             
             <li className="nutriInformation__item">Carbohydrate</li>
             <li className="nutriInformation__info">22.3 g</li>
-            <img src={line} />
+            <img className="nutriInformation__breakLine" src={line} />
             
             <li className="nutriInformation__item">Cholesterol</li>
             <li className="nutriInformation__info">37.4 mg</li>
-            <img src={line} />
+            <img className="nutriInformation__breakLine" src={line} />
         </ul>
             
             <p className="nutriInformation__text">adipiscing elit, 
