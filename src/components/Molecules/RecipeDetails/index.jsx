44abdb39d64ac3b231d91fbf2f79e4f3a578a328
kellycesario <kellycesario@gmail.com>
2022-07-19 17:05:56 -0300

    Improving the RecipeDetails molecule (style and BEM)

--- src/components/Molecules/RecipeDetails/index.jsx
+++ src/components/Molecules/RecipeDetails/index.jsx
@@ -1,66 +1,34 @@
-import printer from '@icons/printer.svg';
-import share from '@icons/share.svg';
-import line from '@icons/line.svg';
-import timer from '@icons/timer.svg';
-import forkKnife from '@icons/forkKnife.svg';
-import JohnSmith from '@img/johnSmithLogo.png';
+import NutriInformation from '@molecules/nutritioninformation'
+import RecipeInformation from '@molecules/recipeinformation'
+
 import RecipeVideo from '@img/recipeDetails-video.png'
 
 import './style.scss'
 
-import NutriInformation from '@molecules/nutritioninformation'
-
 const RecipeDetails = () => {
     return (
-        <section className="recipeDetails">
-            
+        <section className="recipeDetails">            
             <h2 className="recipeDetails__title">Health Japanese Fried Rice</h2>
-        
-        <div className="recipeDetails__infoItems">
-            <img src={JohnSmith} />
-            <p>John Smith</p>
-            <p> 15 March 2022</p>
-            <img src={line} />
-            
-            <img src={timer} />
-            <p>Prep Time</p>
-            <p>15 Minutes</p>
-            <img src={line} />
+            <RecipeInformation />    
+                
+            <div className="recipeDetails__visualArea">
+                <img className="recipeDetails__video" src={RecipeVideo} />
+                <NutriInformation />
+            </div>
 
-            <img src={timer} />
-            <p>Cook Time</p>
-            <p>15 Minutes</p>
-            <img src={line} />
-
-            <img src={forkKnife} />
-            <p>Chicken</p>
-        </div>
-
-        <div className="recipeDetails__share">
-            <img src={printer} /> 
-            <p>Print</p>
-            <img src={share} /> 
-            <p>Share</p>
-        </div>
-
-        <div className="recipeDetails__videoArea">
-            <img className="recipeDetails__video" src={RecipeVideo} />
-            <NutriInformation />
-        </div>
-
-        <p>Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et 
-            dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi 
-            ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit 
-            in voluptate velit esse cillum dolore 
-            eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim 
-            id est laborum.
-        </p>
+            <p className="recipeDetails__endingText">Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et 
+                dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi 
+                ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit 
+                in voluptate velit esse cillum dolore 
+                eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim 
+                id est laborum.
+            </p>
 
         </section>
     );
