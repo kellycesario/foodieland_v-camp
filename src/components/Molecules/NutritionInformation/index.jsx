a8077caf15eda3791d3a443a9ca7a5744fdebbed
kellycesario <kellycesario@gmail.com>
2022-07-19 11:18:10 -0300

    Finishing the static version of the Nutrition Information Card

--- src/components/Molecules/NutritionInformation/index.jsx
+++ src/components/Molecules/NutritionInformation/index.jsx
@@ -6,30 +6,32 @@ const NutriInformation = ()  => {
         <div className="nutriInformation">
             <p className="nutriInformation__title">Nutrition Information</p>
         
-        <div className="nutriInformation__contents">
-            <span className="nutriInformation__item">Calories</span>
-            <span className="nutriInformation__info">219.9 kcal</span>
+        <ul className="nutriInformation__contents">
+            <li className="nutriInformation__item">Calories</li>
+            <li className="nutriInformation__info">219.9 kcal</li>
+            <img src={line} />
+          
+            <li className="nutriInformation__item">Total Fat</li>
+            <li className="nutriInformation__info">10.7 g</li>
             <img src={line} />
             
-            <span className="nutriInformation__item">Total Fat</span>
-            <span className="nutriInformation__info">10.7 g</span>
+            <li className="nutriInformation__item">Protein</li>
+            <li className="nutriInformation__info">7.9 g</li>
             <img src={line} />
             
-            <span className="nutriInformation__item">Protein</span>
-            <span className="nutriInformation__info">7.9 g</span>
+            <li className="nutriInformation__item">Carbohydrate</li>
+            <li className="nutriInformation__info">22.3 g</li>
             <img src={line} />
             
-            <span className="nutriInformation__item">Carbohydrate</span>
-            <span className="nutriInformation__info">22.3 g</span>
+            <li className="nutriInformation__item">Cholesterol</li>
+            <li className="nutriInformation__info">37.4 mg</li>
             <img src={line} />
+        </ul>
             
-            <span className="nutriInformation__item">Cholesterol</span>
-            <span className="nutriInformation__info">37.4 mg</span>
-            <img src={line} />
-            </div>
-            
-            <p className="nutriInformation__text">adipiscing elit, sed do eiusmod tempor incididunt 
+            <p className="nutriInformation__text">adipiscing elit, 
+                sed do eiusmod tempor incididunt 
                 ut labore et dolore magna aliqua. </p>
+
         </div>
     );
 }
