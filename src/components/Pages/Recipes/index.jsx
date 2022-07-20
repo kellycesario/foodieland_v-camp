de46d6cd8f15c0c4b2d8ab6ab97915b9c59ff423
kellycesario <kellycesario@gmail.com>
2022-07-20 18:37:00 -0300

    Organizing the route of the 'Recipes' page

--- src/components/Pages/Recipes/index.jsx
+++ src/components/Pages/Recipes/index.jsx
@@ -1,13 +1,13 @@
+import RecipeDetails from '@organisms/RecipeDetails';
 import Newsletter from "@organisms/Newsletter"
 import CarrouselFood from "@temp/Carrousel";
-import RecipeDetails from '@molecules/RecipeDetails';
 
 const Recipes = () => {
   return (
     <>
-        <RecipeDetails />
-        <Newsletter />
-        <CarrouselFood />
+      <RecipeDetails />
+      <Newsletter />
+      <CarrouselFood />
     </>
   );
 };
