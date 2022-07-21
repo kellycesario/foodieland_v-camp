b56f58c682bda82dc84d7263c2af5b8f7c809b33
kellycesario <kellycesario@gmail.com>
2022-07-21 17:00:14 -0300

    Creating the 'RecipeIngredients' molecule

--- src/components/Molecules/RecipeIngredients/index.jsx
+++ src/components/Molecules/RecipeIngredients/index.jsx
@@ -1,8 +1,78 @@
+import largeLine from '@icons/line-large.svg'
+
 import './style.scss'
 
 const RecipeIngredients = () => {
     return(
+        <section className="recipeIngredients">
+            <h3 className="recipeIngredients__title"> Ingredients </h3>
+            <h4 className="recipeIngredients__mainDish"> For main dish </h4>
+            <fieldset className="recipeIngredients__fieldset">
+                <div className="recipeIngredients__checkboxArea">
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
 
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+                </div>
+            </fieldset>
+
+            <h4 className="recipeIngredients__sauce"> For the sauce</h4>
+
+            <fieldset className="recipeIngredients__fieldset">
+                <div className="recipeIngredients__checkboxArea">
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+
+                    <label className="recipeIngredients__label">
+                        <input type="checkbox" id="checkbox" className="recipeIngredients__checkbox"/>
+                        <span for="checkbox" className="recipeIngredients__item"> 
+                        Lorem ipsum dolor sit amet </span>
+                    </label>
+                    <img className="recipeIngredients__breakLine" src={largeLine} />
+                </div>
+            </fieldset>
+        </section>
     );
 }
 
